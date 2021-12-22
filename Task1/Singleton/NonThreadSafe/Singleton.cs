e31ee76b18d7bf9f73d74873ae716319e9976e40
Arnas <arnas.vaiceliunas@gmail.com>
2021-12-22 16:22:28 +0200

    Task1

--- Task1/Singleton/NonThreadSafe/Singleton.cs
+++ Task1/Singleton/NonThreadSafe/Singleton.cs
@@ -4,12 +4,18 @@ namespace Singleton.NonThreadSafe
 {
     public class Singleton
     {
+        private static Singleton _instance = null;
+        private Singleton()
+        {
+        }
         public static Singleton Instance
         {
             get
             {
-                // TODO: Return single instance, no thread safe
-                throw new NotImplementedException();
+                if (_instance == null)
+                    _instance = new Singleton();
+
+                return _instance;
             }
         }
     }
