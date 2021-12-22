d40ddd90fe75d5d8d1b25e319adcde29b68a2ffa
Arnas <arnas.vaiceliunas@gmail.com>
2021-12-22 16:24:18 +0200

    Task2

--- Task2/Singleton/ThreadSafe/Singleton.cs
+++ Task2/Singleton/ThreadSafe/Singleton.cs
@@ -4,12 +4,22 @@ namespace Singleton.ThreadSafe
 {
     public class Singleton
     {
+        private static Singleton _instance = null;
+        private static readonly object _lock = new object();
+        private Singleton()
+        {
+        }
         public static Singleton Instance
         {
             get
             {
-                // TODO: Return single instance, thread safe with lock
-                throw new NotImplementedException();
+                lock (_lock)
+                {
+                    if (_instance == null)
+                        _instance = new Singleton();
+
+                    return _instance;
+                }
             }
         }
     }
